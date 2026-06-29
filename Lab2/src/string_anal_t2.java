29dc7b1e477884479bea634daf695c1ac3414c54
ay-shifa <shifa.ay12@gmail.com>
2026-06-29 19:59:41 +0500

    Delete Lab2/src directory

--- Lab2/src/string_anal_t2.java
+++
@@ -1,8 +0,0 @@
-public class string_anal_t2 {
-    public static void main(){
-        String my_name = "Shifa Fatima";
-        System.out.println("Length of string is: " + my_name.length());
-        String new_name = my_name.toUpperCase();
-        System.out.println(new_name.indexOf(" "));
-    }
-}
